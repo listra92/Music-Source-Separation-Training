19708d7d6162252cfd97b8177e012180e4eb3f65
listra92 <listra92@gmail.com>
2025-09-22 21:52:17 +0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -66,7 +66,7 @@ def run_folder(model, args, config, device, ckpt_name, verbose: bool = False):
         start_time = time.time()
         print(f"Processing track: {path}")
         try:
-            mix, sr = librosa.load(path, sr=sample_rate, mono=False)
+            mix, sr = librosa.load(path, sr=sample_rate, mono=not config.model['stereo'])
         except Exception as e:
             print(f'Cannot read track: {format(path)}')
             print(f'Error message: {str(e)}')
