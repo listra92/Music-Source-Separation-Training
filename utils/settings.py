af87ce4d93b65503984ee9458bb17264e42cd397
listra92 <listra92@gmail.com>
2026-01-01 15:39:00 +0700

    Update settings.py

--- utils/settings.py
+++ utils/settings.py
@@ -285,14 +285,20 @@ def get_model_from_config(model_type: str, config_path: str) -> Tuple[nn.Module,
         from models.torchseg_models import Torchseg_Net
         model = Torchseg_Net(config)
     elif model_type == 'mel_band_roformer':
+        try:
             from models.bs_roformer import MelBandRoformer
             model = MelBandRoformer(**dict(config.model))
+        except Exception as e:
+            pass
     elif model_type == 'mel_band_roformer_experimental':
         from models.bs_roformer.mel_band_roformer_experimental import MelBandRoformer
         model = MelBandRoformer(**dict(config.model))
     elif model_type == 'bs_roformer':
+        try:
             from models.bs_roformer import BSRoformer
             model = BSRoformer(**dict(config.model))
+        except Exception as e:
+            pass
     elif model_type == 'bs_roformer_experimental':
         from models.bs_roformer.bs_roformer_experimental import BSRoformer
         model = BSRoformer(**dict(config.model))
